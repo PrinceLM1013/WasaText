4e5277f0abfd5b13f76e65152db6d0a8c9e18f07
PrinceLM1013 <prajapati.1986763@studenti.uniroma1.it>
2024-12-06 18:38:39 +0100

    Backend

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -13,5 +13,24 @@ func (rt *_router) Handler() http.Handler {
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
 
+	// User routes
+	rt.router.POST("/session", rt.wrap(rt.Dologin))
+	rt.router.PUT("/users/me/name", rt.wrap(rt.setMyUserName))
+	rt.router.PUT("/users/me/photo", rt.wrap(rt.setMyPhoto))
+
+	// Conversation routes
+	rt.router.GET("/conversations", rt.wrap(rt.getMyConversations))
+	rt.router.GET("/conversations/:id", rt.wrap(rt.getConversation))
+
+	// Message routes
+	rt.router.POST("/messages", rt.wrap(rt.sendMessage))
+	rt.router.POST("/messages/:id/forward", rt.wrap(rt.forwardMessage))
+	rt.router.POST("/messages/:id/comment", rt.wrap(rt.commentMessage))
+	rt.router.DELETE("/messages/:id/comment", rt.wrap(rt.uncommentMessage))
+
+	// Group routes
+	rt.router.POST("/groups/:id/leave", rt.wrap(rt.leaveGroup))
+	rt.router.PUT("/groups/:id/photo", rt.wrap(rt.setGroupPhoto))
+
 	return rt.router
 }
